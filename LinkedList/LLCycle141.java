129673aacf010340a90fba02dd51e3cbc7edd3bb
jahnavi2506-stack <ayinjahnavi@gmail.com>
2026-04-03 22:49:22 +0530

    feat: implement linked list cycle

--- LinkedList/LLCycle141.java
+++ LinkedList/LLCycle141.java
@@ -3,11 +3,11 @@ public class Solution {
         ListNode slow = head;
         ListNode fast = head;
 
-        while(fast != null && fast.next != null) { //we must check fast and fast.next before accessing fast.next.next to avoid NullPointerException
+        while(fast != null && fast.next != null) { // we must check fast and fast.next before accessing fast.next.next to avoid NullPointerException
             slow = slow.next;
             fast = fast.next.next;
 
-            if(slow == fast) return true;//both points to same node, means there is a cycle
+            if(slow == fast) return true;// both points to same node, means there is a cycle
         }
         return false;
     }
